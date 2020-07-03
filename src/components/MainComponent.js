7b5599503029e772ca114da6d6f5339552f7d192
ParthArora <partharora.india@gmail.com>
2020-07-04 00:13:49 +0530

    Assignment Week-2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,6 +11,7 @@ import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -48,6 +49,7 @@ class Main extends Component {
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
+              <Route exact path='/aboutus' component={()=> <About leaders={this.state.leaders} />} />
               <Redirect to="/home" />
           </Switch>
         <Footer />
